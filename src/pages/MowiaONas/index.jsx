5ec2562250d127ed16d3dd71601ce829acd9af42
Kosma Cieliczko <kosmacieliczko@Kosmas-MacBook-Air.local>
2024-06-18 14:50:16 +0200

    cosmetic changes

--- src/pages/MowiaONas/index.jsx
+++ src/pages/MowiaONas/index.jsx
@@ -10,13 +10,13 @@ export default function TalkingAboutUs() {
         <Container p={4} maxW={'5xl'} bg={'transparent'} my={4} rounded={'lg'}>
           <Text fontSize="4xl" fontWeight={'bold'} color={"blue.400"} >Uliczne Podcasty</Text>
           <Text fontSize="2xl" color={"blue.400"}>Mowia o nas </Text>
-          <Button as={Link} to="player/1" colorScheme="blue" m={2}>Ulice Poznania - Odcinek 1</Button>
-          <Button as={Link} to="player/2" colorScheme="blue" m={2}>Ulice Poznania - Odcinek 2</Button>
-          <Button as={Link} to="player/3" colorScheme="blue" m={2}>Ulice Poznania - Odcinek 3</Button>
-          <Button as={Link} to="player/6" colorScheme="blue" m={2}>Poznań rzeczny</Button>
-          <Button as={Link} to="player/7" colorScheme="blue" m={2}>Poznań wyspowy</Button>
-          <Button as={Link} to="player/4" colorScheme="blue" m={2}>Ulice Poznania - Odcinek 6</Button>
-          <Button as={Link} to="player/5" colorScheme="blue" m={2}>Ulice Poznania - Odcinek 7</Button>
+          <Button as={Link} to="player/1" colorScheme="blue" m={2}>Odcinek 1 - Ulice Poznania</Button>
+          <Button as={Link} to="player/2" colorScheme="blue" m={2}>Odcinek 2 - Ulice Poznania</Button>
+          <Button as={Link} to="player/3" colorScheme="blue" m={2}>Odcinek 3 - Ulice Poznania</Button>
+          <Button as={Link} to="player/6" colorScheme="blue" m={2}>Odcinek 4 - Poznań rzeczny</Button>
+          <Button as={Link} to="player/7" colorScheme="blue" m={2}>Odcinek 5 - Poznań wyspowy</Button>
+          <Button as={Link} to="player/4" colorScheme="blue" m={2}>Odcinek 6 - Ulice Poznania</Button>
+          <Button as={Link} to="player/5" colorScheme="blue" m={2}>Odcinek 7 - Ulice Poznania</Button>
         </Container>
       </Flex>
       <Routes>
