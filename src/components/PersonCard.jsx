a48308c3d7df539b8c4ebd4004dc8159ea911c9f
Kosma Cieliczko <kosmacieliczko@Kosmas-MacBook-Air.local>
2024-06-20 15:45:57 +0200

    changed presonal car

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,93 @@
+// PersonCard.jsx
+import React, { useState } from "react";
+import { Box, VStack, Image, Text, Center } from "@chakra-ui/react";
+import ReactCardFlip from "react-card-flip";
+
+export default function PersonCard({ item, handleBoxClick }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <Box
+      w={"200px"}
+      onClick={() => handleBoxClick(item)}
+      onMouseEnter={handleFlip}
+      onMouseLeave={handleFlip}
+      _hover={{
+        cursor: "pointer",
+        transform: "scale(1.05)",
+        transition: "all 0.2s",
+      }}
+    >
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <VStack
+          borderRadius={"5px"}
+          bg="white"
+          minH={"370px"}
+          boxShadow="md"
+          minW={"230px"}
+          border={"8px solid"}
+          borderColor={"blue.400"}
+          bgColor={"blue.400"}
+        >
+          <Box
+            w={"100%"}
+            h={"100%"}
+            bgColor={"blue.100"}
+            p={5}
+            borderTopLeftRadius={"5px"}
+            borderTopRightRadius={"5px"}
+          >
+            <Center>
+              <Box boxSize={"180px"} bgColor={"transparent"}>
+                <Image
+                  boxSize="180px"
+                  w={"180px"}
+                  objectFit="cover"
+                  src={item.image}
+                  alt={item.name}
+                  borderRadius={"full"}
+                />
+              </Box>
+            </Center>
+          </Box>
+          <Text
+            mx={1}
+            textAlign={"center"}
+            fontWeight={"bold"}
+            fontSize={"20px"}
+            bgColor={"blue.400"}
+            color={"white"}
+          >
+            {item.name}
+          </Text>
+          <Text mx={1} textAlign={"center"} color={"white"}>
+            {item.group}
+          </Text>
+        </VStack>
+        <VStack
+          borderRadius={"5px"}
+          bg="white"
+          minH={"370px"}
+          boxShadow="md"
+          minW={"230px"}
+          border={"8px solid"}
+          borderColor={"blue.400"}
+          bgColor={"blue.400"}
+        >
+            <Text color={'white'} fontWeight={'bold'} textAlign={'center'}>
+                {item.name}
+            </Text>
+          <Text px={5} color={'white'}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            lacinia malesuada libero tempus congue. Duis et lacus et massa
+            rutrum sagittis. Suspendisse dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Text>
+        </VStack>
+      </ReactCardFlip>
+    </Box>
+  );
+}
--- src/pages/Home/SocialMedia.jsx
+++ src/pages/Home/SocialMedia.jsx
@@ -1,20 +1,7 @@
-import {
-  Box,
-  VStack,
-  Image,
-  Text,
-  Flex,
-  Center,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  Stack,
-  ModalCloseButton,
-  ModalBody,
-  useDisclosure,
-  Spacer,
-} from "@chakra-ui/react";
+import { Center, useDisclosure, Grid } from "@chakra-ui/react";
 import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import PersonCard from "../../components/PersonCard";
 
 export default function Team() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -64,109 +51,54 @@ export default function Team() {
     {
       name: "Paweł Cieliczko",
       image:
-        "https://wspolnasprawa.blob.core.windows.net/wspolnasprawaphotos/okladka_Czerwcowe ulice.jpg",
+        "https://wspolnasprawa.blob.core.windows.net/wspolnasprawaphotos/Fronczak.png",
       group: "Stowarzyszenie Husaria",
       description:
         "Paweł Cieliczko - prezes zarządu Fundacji Kochania Poznania. Historyk, publicysta, autor książek, wykładowca, pasjonat historii Poznania i Wielkopolski.",
     },
-    // Add more items here
+    {
+      name: "Paweł Cieliczko",
+      image:
+        "https://wspolnasprawa.blob.core.windows.net/wspolnasprawaphotos/Fronczak.png",
+      group: "Stowarzyszenie Husaria",
+      description:
+        "Paweł Cieliczko - prezes zarządu Fundacji Kochania Poznania. Historyk, publicysta, autor książek, wykładowca, pasjonat historii Poznania i Wielkopolski.",
+    },
+    {
+      name: "Paweł Cieliczko",
+      image:
+        "https://wspolnasprawa.blob.core.windows.net/wspolnasprawaphotos/Fronczak.png",
+      group: "Stowarzyszenie Husaria",
+      description:
+        "Paweł Cieliczko - prezes zarządu Fundacji Kochania Poznania. Historyk, publicysta, autor książek, wykładowca, pasjonat historii Poznania i Wielkopolski.",
+    },
+    {
+      name: "Paweł Cieliczko",
+      image:
+        "https://wspolnasprawa.blob.core.windows.net/wspolnasprawaphotos/Fronczak.png",
+      group: "Stowarzyszenie Husaria",
+      description:
+        "Paweł Cieliczko - prezes zarządu Fundacji Kochania Poznania. Historyk, publicysta, autor książek, wykładowca, pasjonat historii Poznania i Wielkopolski.",
+    },
+    {
+      name: "Paweł Cieliczko",
+      image:
+        "https://wspolnasprawa.blob.core.windows.net/wspolnasprawaphotos/Fronczak.png",
+      group: "Stowarzyszenie Husaria",
+      description:
+        "Paweł Cieliczko - prezes zarządu Fundacji Kochania Poznania. Historyk, publicysta, autor książek, wykładowca, pasjonat historii Poznania i Wielkopolski.",
+    },
   ];
-
-  const handleBoxClick = (item) => {
-    setSelectedItem(item);
-    onOpen();
-  };
 
   return (
     <Center>
-      <Box
-        maxW="100%"
-        overflowX="scroll"
-        overflowY="hidden"
-        marginX={"50px"}
-        p={"20px"}
-        marginY={"20px"}
-        backgroundColor={"blue.400"}
-      >
-        <Flex direction="row" spacing={10}>
-          {items.map((item, index) => (
-            <Box
-              w={"270px"}
-              p={5}
-              onClick={() => handleBoxClick(item)}
-              _hover={{
-                cursor: "pointer",
-                transform: "scale(1.05)",
-                transition: "all 0.2s",
-              }}
-            >
-              <VStack
-                key={index}
-                borderRadius={"5px"}
-                bg="white"
-                minH={"300px"}
-                p={4}
-                boxShadow="md"
-                minW={"250px"}
-              >
-                <Image
-                  mt={"10px"}
-                  boxSize="150px"
-                  minW={"150px"}
-                  objectFit="cover"
-                  src={item.image}
-                  alt={item.name}
-                  borderRadius="full" // make the image circular
-                />
-                <Text
-                  textAlign={"center"}
-                  fontWeight={"semi-bold"}
-                  fontSize={"20px"}
-                >
-                  {item.name}
-                </Text>
-                <Text textAlign={"center"}>{item.group}</Text>
-              </VStack>
-            </Box>
-          ))}
-        </Flex>
-      </Box>
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        size={"3xl"}
-        motionPreset="slideInBottom"
-        isCentered
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalCloseButton />
-          <ModalBody>
-            <Stack
-              direction={{ base: "column", md: "row" }}
-              spacing={4}
-              alignContent={"start"}
-              p={5}
-            >
-              <Image
-                boxSize="200px"
-                objectFit="cover"
-                src={selectedItem?.image}
-                alt={selectedItem?.name}
-                borderRadius="full"
-              />
-              <Spacer />
-              <VStack w={"100%"} alignItems={"start"}>
-                <Text fontSize={"2xl"} textAlign="center">
-                  {selectedItem?.name}
-                </Text>
-                <Text fontWeight={"bold"}>{selectedItem?.group}</Text>
-                <Text>{selectedItem?.description}</Text>
-              </VStack>
-            </Stack>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <Grid templateColumns="repeat(5, 1fr)" rowGap={4} gap={10} maxW="100%">
+        {items.map((item, index) => (
+          <PersonCard item={item} />
+        ))}
+      </Grid>
+
+      {/* ...rest of your code... */}
     </Center>
   );
 }
